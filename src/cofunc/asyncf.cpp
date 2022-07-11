e56f394b1cc712e66030ea6ecd7c1956ff8a0bb3
wlt <wlt15@tsinghua.org.cn>
2022-07-12 01:20:48 +0800

    fix: unique lock async::suspend_

--- src/cofunc/asyncf.cpp
+++ src/cofunc/asyncf.cpp
@@ -13,12 +13,14 @@ std::unordered_map<Async::Handler, Async::Pstack, Async::Hash>
 void Async::start() {
   auto pstk = std::make_shared<std::stack<Handler>>();
   pstk->push(handler_);
-  std::unique_lock lock(class_);
-  handler_stack_[handler_] = pstk;
+  {
+    std::unique_lock lock(class_);
+    handler_stack_[handler_] = pstk;
+  }
 }
 
 void Async::resume() {
-  Async::Pstack pstk;
+  Pstack pstk;
   {
     std::shared_lock lock(class_);
     pstk = handler_stack_.at(handler_);
@@ -34,9 +36,9 @@ void Async::resume() {
 bool Async::done() { return !handler_ || handler_.done(); }
 
 void Async::suspend_(Handler caller) {
-  Async::Pstack pstk;
+  Pstack pstk;
   {
-    std::shared_lock lock(class_);  // read lock is enough
+    std::unique_lock lock(class_);
     pstk = handler_stack_.at(caller);
     pstk->push(handler_);
     handler_stack_[handler_] = pstk;
